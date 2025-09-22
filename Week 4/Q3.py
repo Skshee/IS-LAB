ba8894c1bf89530935d542d38d5714892ae89314
Skshee <suvanshee@gmail.com>
2025-09-23 00:39:35 +0530

    Update Q3.py

--- Week 4/Q3.py
+++ Week 4/Q3.py
@@ -1,4 +1,8 @@
 '''
+Given an ElGamal encryption scheme with a public key (p, g, h) and a private key
+x, encrypt the message "Confidential Data". Then decrypt the ciphertext to retrieve
+the original message. 
+
     Parameters:
     - p (int): A prime number
     - alpha (int): A primitive root modulo p
@@ -37,5 +41,6 @@ print(f"Encrypted message: c1 = {c1}, c2 = {c2}")
 decrypted_m = elgamal_decryption(p, r, c1, c2)
 original_message = long_to_bytes(decrypted_m).decode()
 print(f"Decrypted message: {original_message}")
+
 
 
