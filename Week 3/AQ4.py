fb14ee98126cd8ebf193fb85920d7b3e552c69c9
Skshee <suvanshee@gmail.com>
2025-09-23 01:06:07 +0530

    Create AQ4.py

---
+++ Week 3/AQ4.py
@@ -0,0 +1,20 @@
+'''
+Encrypt the message "Secure Communication" using DES in Cipher Block Chaining
+(CBC) mode with the key "A1B2C3D4" and an initialization vector (IV) of "12345678".
+Provide the ciphertext and then decrypt it to retrieve the original message.
+'''
+from Crypto.Cipher import DES
+from Crypto.Util.Padding import pad, unpad
+
+key = b"A1B2C3D4"
+iv = b"12345678"
+message = b"Secure Communication"
+
+cipher = DES.new(key, DES.MODE_CBC, iv)
+
+ciphertext = cipher.encrypt(pad(message, DES.block_size))
+print("DES CBC Ciphertext:", ciphertext.hex())
+
+decipher = DES.new(key, DES.MODE_CBC, iv)
+decrypted = unpad(decipher.decrypt(ciphertext), DES.block_size)
+print("DES CBC Decrypted:", decrypted.decode())
